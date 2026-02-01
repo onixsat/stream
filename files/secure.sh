72a9c526ebfe0ae13f16d9f27d5a023fce9c4eb1
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-01 16:46:07 +0000

    lk

--- files/secure.sh
+++ files/secure.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -x
+#curl --verbose http://172.17.205.201/login
+#curl --verbose -u user:password http://172.17.205.201/login
+#curl --verbose -u onix:onix http://172.17.205.201/login
+# curl --verbose -u  http://172.17.205.201/index.php
+# curl --verbose -u onix:onix http://172.17.205.201/index.php
+# curl -4 --head http://172.17.205.201/index.php
+# curl -4 --head http://172.17.205.201
+# curl -4 --head http://ninjapardal.xyz
+# curl -4 --head https://ninjapardal.xyz
+# curl -4 --head http://ninjapardal.xyz:8080/get.php
+# curl -4 --head https://ninjapardal.xyz:8443/get.php
+
+#location /login {
+#      auth_basic "Protected area!";
+#      auth_basic_user_file /etc/nginx/users;
+#}
 
 if [ ! -f /etc/nginx/users ]
 then
